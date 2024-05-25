7d78aaf68fd1bd361196329ef089c226094c958f
kmh990303 <63326675+kmh990303@users.noreply.github.com>
2024-05-25 16:50:27 +0900

    Update Player.jsx

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react"
 
-export default function Player({ initialName, symbol }) {
+export default function Player({ initialName, symbol, isActive, onChangeName }) {
     const [playerName, setPlayerName] = useState(initialName);
 
     const [isEditing, setIsEditing] = useState(false);
 
     const handleEditClick = () => {
-        setIsEditing((editing) => !isEditing);
+        setIsEditing((editing) => !editing);
+        if (isEditing) {
+            onChangeName(symbol, playerName)
+        }
     }
 
 
@@ -24,7 +27,7 @@ export default function Player({ initialName, symbol }) {
     }
 
     return (
-        <li>
+        <li className={isActive ? 'active' : undefined}>
             <span className="player">
                 {editablePlayerName}
                 <span className="player-symbol">{symbol}</span>
