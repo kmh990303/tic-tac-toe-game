11acef175601275a3d5cba00ff79fc84f035ba8b
kmh990303 <63326675+kmh990303@users.noreply.github.com>
2024-05-25 16:50:05 +0900

    Update GameBoard.jsx

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,30 +1,33 @@
-import { useState } from "react";
 
-const initialGameBoard = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null],
-];
 
-export default function GameBoard() {
-    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+export default function GameBoard({ onSelectSquare, board }) {
+   
 
-    function handleSelectSqaure(rowIndex, colIndex) {
-        setGameBoard((prevGameBoard) => { 
-           const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-           updatedBoard[rowIndex][colIndex] = 'X';
-           return updatedBoard;
-        });
-    }
+    // const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+    // function handleSelectSqaure(rowIndex, colIndex) {
+    //     setGameBoard((prevGameBoard) => {
+    //         const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+    //         updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+    //         return updatedBoard;
+    //     });
+
+    //     onSelectSquare();
+    // }
 
     return (
         <ol id="game-board">
-            {gameBoard.map((row, rowIndex) => (
+            {board.map((row, rowIndex) => (
                 <li key={rowIndex}>
                     <ol>
                         {row.map((playerSymbol, colIndex) => (
                             <li key={colIndex}>
-                                <button onClick={() => handleSelectSqaure(rowIndex, colIndex)}>{playerSymbol}</button>
+                                <button
+                                    onClick={() => onSelectSquare(rowIndex, colIndex)}
+                                    disabled={playerSymbol !== null}
+                                >
+                                    {playerSymbol}
+                                </button>
                             </li>
                         ))}
                     </ol>
