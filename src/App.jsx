7b3f12cc71029083ec47f8c8bf3f4504788c081d
Minhyuk Kim <labcs@naver.com>
2024-09-11 21:06:35 +0900

    add file & edit README

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Player from "./components/Player"
 import GameBoard from "./components/GameBoard"
 import { useState } from "react"
 import Log from "./components/Log"
-import { WINNING_COMBINATIONS } from "./winning-conbinations"
+import { WINNING_COMBINATIONS } from "./components/winning-combinations"
 import GameOver from "./components/GameOver"
 
 const PLAYERS = {
